ab947bce85204fb7a1a43c8494740573d67889f8
vitaliykulishov <vitalii.kulishov@gmail.com>
2025-09-11 00:39:34 +0300

    add swiper part 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import './styles/main.scss'
+import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.slider-teacher', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  modules: [Navigation, Pagination],
+
+});
