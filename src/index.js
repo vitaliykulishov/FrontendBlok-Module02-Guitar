85c3567f8478f30ac7709aad0ca934d0b313b715
vitaliykulishov <vitalii.kulishov@gmail.com>
2025-09-14 23:19:10 +0300

    add accordion

--- src/index.js
+++ src/index.js
@@ -17,3 +17,18 @@ const swiper = new Swiper('.slider-teacher', {
   modules: [Navigation, Pagination],
 
 });
+
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var panel = this.nextElementSibling;
+   if (panel.style.maxHeight) {
+     panel.style.maxHeight = null;
+   } else {
+     panel.style.maxHeight = panel.scrollHeight + "px";
+   }
+ });
+}
